314c0839713978e3ca6a0e5c2adad98d7021683b
Лев Чернин <cherninleva@gmail.com>
2024-06-01 01:46:29 +0300

    feat: modal without a working select

---
+++ source/js/modal-tel.js
@@ -0,0 +1,36 @@
+(function () {
+  const baseCountryCode = '+7';
+  const baseMatrix = ' (___) ___ __ __';
+
+  const onPhoneInputInput = (e) => {
+    const matrix = `${baseCountryCode}${baseMatrix}`;
+    const val = e.target.value.replace(/\D/g, '');
+    let i = 0;
+    e.target.value = matrix.replace(/./g, (a) => {
+      if (/[_\d]/.test(a) && i < val.length) {
+        return val.charAt(i++);
+      } else if (i >= val.length) {
+        return '';
+      } else {
+        return a;
+      }
+    });
+  };
+
+  const onPhoneInputBlur = (e) => {
+    if (e.target.value === baseCountryCode) {
+      e.target.value = '';
+    }
+  };
+
+  const initPhoneInput = () => {
+    const phoneInput = document.getElementById('phone');
+    phoneInput.addEventListener('input', onPhoneInputInput);
+    phoneInput.addEventListener('blur', onPhoneInputBlur);
+    phoneInput.addEventListener('focus', () => {
+      phoneInput.value = baseCountryCode;
+    });
+  };
+
+  initPhoneInput();
+})();
---
+++ source/js/modal.js
@@ -0,0 +1,62 @@
+(function () {
+  const body = document.querySelector('body');
+  const modal = document.querySelector('.modal');
+  const submitButton = modal.querySelector('.modal__submit-button');
+  const openButton = document.querySelector('.about__button');
+  const closeButton = document.querySelector('.modal__close-button');
+  const overlay = document.querySelector('.header__overlay');
+
+  const openModal = (evt) => {
+    evt.preventDefault();
+    modal.classList.toggle('is-open');
+    overlay.classList.toggle('is-open');
+    body.style.overflow = 'hidden';
+  };
+
+  const closeModal = (evt) => {
+    evt.preventDefault();
+    modal.classList.remove('is-open');
+    overlay.classList.remove('is-open');
+    body.style.overflow = 'auto';
+  };
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      modal.classList.remove('is-open');
+      overlay.classList.remove('is-open');
+      body.style.overflow = 'auto';
+    }
+  });
+
+  document.addEventListener('click', (event) => {
+    if (!event.target.closest('.modal') && event.target !== openButton) {
+      modal.classList.remove('is-open');
+      overlay.classList.remove('is-open');
+      body.style.overflow = 'auto';
+    }
+  });
+
+  const toggleSubmitButton = (isDisabled) => {
+    submitButton.disabled = isDisabled;
+  };
+
+  const onClick = () => {
+    if (!modal.classList.contains('modal__eror')) {
+      modal.classList.add('modal__error');
+    }
+  };
+
+  const onSubmit = async () => {
+    toggleSubmitButton(true);
+    modal.reset();
+    setTimeout(() => {
+      toggleSubmitButton(false);
+    }, 1000);
+  };
+
+  closeButton.addEventListener('click', closeModal);
+  openButton.addEventListener('click', openModal);
+  modal.addEventListener('submit', onSubmit);
+  submitButton.addEventListener('click', onClick);
+})();
+
